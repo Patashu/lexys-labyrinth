b6931c72086d9177fe879af374745a20b3bd57f0
Timothy Stiles <Patashu@hotmail.com>
2021-03-30 23:03:22 +1100

    clarify some descriptions

--- js/main-editor.js
+++ js/main-editor.js
@@ -2299,11 +2299,11 @@ const EDITOR_TILE_DESCRIPTIONS = {
     },
     cloud_water: {
         name: "Cloud (Water)",
-        desc: "Acts like water. Hides and negates the properties of the terrain underneath (but NOT the item). When filled in, turns into Cloud (Dirt) instead of Dirt.",
+        desc: "Acts like water. Hides and negates the properties of the terrain underneath (but NOT the item). Underlying terrain is erased by swimming actors. When filled by a block, turns into Cloud or Cloud (Dirt) instead of Floor/Dirt.",
     },
     hidden_item: {
         name: "Hidden item",
-        desc: "Hides and negates the properties of the item underneath until after the first time it's stepped on.",
+        desc: "Hides and negates the properties of the item underneath until after the first time it's stepped on. When stepped on by a non-block non-player actor, is erased.",
     },
 };
 
